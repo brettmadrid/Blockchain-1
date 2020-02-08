ceb223f105aa4a36532f5619c09c30b7e4bdd7fe
brettmadrid <brettmadrid9@gmail.com>
2020-02-08 15:40:40 -0800

    created React client and added Form.js

---
+++ wallet/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./NEO.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>New Neo Coin Wallet</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
